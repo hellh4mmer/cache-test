3eb0df99555576900e68c6e74f50eb77e00c4e2c
Mohammad Aimaan bin Abdul Latif <draimaan@moh.gov.my>
2025-05-20 09:25:30 +0800

    debug souin

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
-// server.js
+// index.ts
 import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  console.log('Request received');
-  res.set('Cache-Control', 'public, max-age=60');
-  res.send('Hello! ' + new Date().toISOString());
+  const body = `Hello! ${new Date().toISOString()} - ${Math.random().toString(36).slice(2)}\n`;
+  console.log('Responding with body of length:', body.length);
+  res.set('Cache-Control', 'public');
+  res.type('text/plain').send(body);
 });
+
 
 app.listen(5000, () => console.log('App listening on port 5000!'));
